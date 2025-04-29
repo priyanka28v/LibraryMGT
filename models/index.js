595bf5fcd81a04e1243b300e5890f50dee615446
priyanka <priyankabhola0803@gmail.com>
2025-04-29 19:13:20 +0530

    Remove extra line

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const Author=require('./Authors')
 const Book=require('./Books')
-//const user=require('./login')
 const User=require('./signups')
+
 module.exports={Author,Book,User};
-// module.exports=Book
+
 
